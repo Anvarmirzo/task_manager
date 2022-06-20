2cd599934abbb80f875b25c770c38c05346a0e2e
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-06-20 17:13:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,20 +12,6 @@ const api = '/api/v1';
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.all('*', function (req, res, next) {
-  // Устанавливаем заголовок запроса, чтобы разрешить междоменный
-  res.header('Access-Control-Allow-Origin', '*');
-  // Устанавливаем все поля заголовка, поддерживаемые сервером
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild, sessionToken');
-  // Устанавливаем методы для всех междоменных запросов, поддерживаемых сервером
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (req.method.toLowerCase() == 'options') {
-      res.send(200);  // Позволяем опциям запрашивать быстрое завершение
-  } else {
-      next();
-  }
-});
-
 // routers
 app.use(`${api}/tasks`, tasksRouter);
 app.use(notFound);
